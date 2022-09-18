6c47f58a23b75cbbcb8ddb3644e65810cb0753cc
waleriQ <interceptor55@icloud.com>
2022-09-18 23:28:36 +0600

    writing burger js script

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-(function () {
-     const burger = document.querySelector('.burger');
-burger.addEventListener('click', () => {
-     burger.classList.toggle('burger_active');
-}) }() )
+// (function () {
+//      const burger = document.querySelector('.burger');
+// burger.addEventListener('click', () => {
+//      burger.classList.toggle('burger_active');
+// }) }() )
+//
+//
 
+let person = {
+     name: 'Valter',
+     FullAge: {
+          age: 26,
+          YearBorn: 1994,
+     },
+     Gender: 'female'
+}
+
+let PersonClone = Object.assign({}, person);
 
